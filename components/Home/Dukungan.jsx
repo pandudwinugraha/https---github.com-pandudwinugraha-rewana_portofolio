613f989bf8eb5b1780d1caff4ea24583b0770439
pandudwinugraha <pandudwinugraha01@gmail.com>
2024-02-10 09:00:56 +0700

    coba test

--- components/Home/Dukungan.jsx
+++ components/Home/Dukungan.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { Poppins } from "next/font/google";
-import Logo1 from "@/public/Logo1.png";
-import Logo2 from "@/public/Logo2.png";
-import Logo3 from "@/public/Logo3.png";
-import Logo4 from "@/public/Logo4.png"
-import Logo5 from "@/public/Logo5.png";
-import Logo6 from "@/public/Logo6.png";
+import { Poppins } from "next/font";
+import Logo1 from "../../public/Logo1.png";
+import Logo2 from "../../public/Logo2.png";
+import Logo3 from "../../public/Logo3.png";
+import Logo4 from "../../public/Logo4.png";
+import Logo5 from "../../public/Logo5.png";
+import Logo6 from "../../public/Logo6.png";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "600" });
 
@@ -20,7 +20,10 @@ const Dukungan = () => {
           Didukung oleh
         </h2>
         <p className="text-center max-w-[555px] mx-auto">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec consequat sapien. Nulla facilisi. Ut vehicula fermentum libero, vel ultricies leo molestie sit amet. Integer consequat libero at libero fermentum, nec ultricies nulla ullamcorper. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+          consequat sapien. Nulla facilisi. Ut vehicula fermentum libero, vel
+          ultricies leo molestie sit amet. Integer consequat libero at libero
+          fermentum, nec ultricies nulla ullamcorper.
         </p>
         <div className="grid grid-cols-3 gap-[21px] mt-8 items-center">
           <div className="flex justify-center">
